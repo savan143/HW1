f776e3dddad1f1eda1bb042edb2e4e5b7360b6c4
Savan Ramparaiya <savan.rampariya14302@gmail.com>
2016-01-25 17:01:54 -0800

    First App on Heroku

---
+++ Calculator.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+ <input type="text" name="value"><br>
+<br>
+<table>
+<tr>
+<td colspan = 2 rowspan 1 >
+<button type="button" >clear</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >=</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >+/-</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >.</button></td>
+</tr>
+<tr>
+<td colspan = 1 rowspan 1 >
+<button type="button" >*</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >/</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >+</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >-</button></td>
+</tr>
+<tr>
+<td colspan = 1 rowspan 1 >
+<button type="button" >1</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >2</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >3</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >4</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >5</button></td>
+</tr>
+<tr>
+<td colspan = 1 rowspan 1 >
+<button type="button" >6</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >7</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >8</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >9</button></td>
+<td colspan = 1 rowspan 1 >
+<button type="button" >0</button></td>
+</tr>
+</table>
+</br>
+</body>
+</html>
---
+++ Table.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+table, td, th {
+    border: 1px Solid black;
+border-collapse: collapse;
+
+    text-align: center;
+}
+</style>
+</head>
+
+<table>
+  <tr>
+    <th colspan=4 rowspan=1>HTML5</th>
+</tr>
+<tr>
+    <th colspan=4 rowspan=1>Table</th>
+  </tr>
+  <tr>
+    <td   colspan=2 rowspan=1>Name</td>
+    <td colspan=2 rowspan=1 >Address</td>
+  </tr>
+  <tr>
+    <td colspan=1 rowspan=1>FirstName</td>
+  <td colspan=1 rowspan=1>LastName</td>
+  <td colspan=1 rowspan=1>State</td>
+  <td colspan=1 rowspan=1>City</td>
+    
+  </tr>
+ <tr>
+    <td colspan=1 rowspan=1>JOHN</td>
+  <td colspan=1 rowspan=1>DOE</td>
+  <td colspan=1 rowspan=1>CA</td>
+  <td colspan=1 rowspan=1>Fremont</td>
+    
+  </tr>
+
+<tr>
+    <td colspan=1 rowspan=1>EVE</td>
+  <td colspan=1 rowspan=1>JACKSON</td>
+  <td colspan=1 rowspan=1>CA</td>
+  <td colspan=1 rowspan=1>San Jose</td>
+    
+  </tr>
+
+<tr>
+    <td colspan=1 rowspan=1>ADAM</td>
+  <td colspan=1 rowspan=1>SMITH</td>
+  <td colspan=1 rowspan=1>CA</td>
+  <td colspan=1 rowspan=1>Milpitas</td>
+    
+  </tr>
+</table>
+
+</body>
+</html>
